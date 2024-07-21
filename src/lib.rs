eff53dec33d5c734c99507f094044091fe5288e8
Jan Żukowski <janzukowski@proton.me>
2024-07-21 10:40:55 +0200

    update: added append_field_names macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
 pub mod config;
 pub mod request;
 pub mod response;
+
+
+mod lib {
+    macro_rules! append_field_names {
+        (pub struct $name:ident { $(pub $fname:ident : $ftype:ty),* }) => {
+            pub struct $name {
+                pub $($fname : $ftype),*
+            }
+    
+            impl $name {
+                pub fn field_names() -> &'static [&'static str] {
+                    static NAMES: &'static [&'static str] = &[$(stringify!($fname)),*];
+                    NAMES
+                }
+            }
+        }
+    }
+
+    pub(crate) use append_field_names;    // <-- the trick
+}
