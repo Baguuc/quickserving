317787c3ac56dba39b84575a748b21d942be434d
baguuc <janzukowski@proton.me>
2025-03-18 07:16:27 +0100

    update: merged the cli into this repository

--- README.md
+++ README.md
@@ -1,15 +1,9 @@
-## Quickserving Core
-
-Core library for other quickserving apps.
+## Quickserving
+A configurable rust http folder server
 
 ## Installation
-
 Add quickserving-core to Cargo.toml as a dependency:
-
 ```TOML
 quickserving-core = { git = "https://github.com/Baguuc/quickserving-core.git" }
 ```
 
-## Docs
-
-Docs avaible [here](./DOCS.md)
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use clap::Parser;
+use log::error;
+use quickserving_core::{config::Config, server::listen};
+use simple_logger::SimpleLogger;
+
+#[derive(Parser, Debug, Clone)]
+#[command(version, about = "A simple HTTP server.")]
+struct CLIConfig {
+    #[arg(long, default_value = "8080", help = "The port that server will be listening for requests on.")]
+    port: u16,
+    #[arg(long, default_value = ".", help = "The directory that will be served.")]
+    directory: String,
+    #[arg(long, default_value = "index.html", help = "The file that will be read from requested path when user requests url ending with '/'.")]
+    index: String,
+    #[arg(long, default_value = "404.html", help = "The file that will be served when the file requested by user is not avaible.")]
+    not_found: String
+}
+
+
+fn main() {
+    SimpleLogger::new().init();
+
+    let cli_config = CLIConfig::parse();
+
+    let setup = listen(Config {
+        port: cli_config.port,
+        directory: cli_config.directory,
+        index_file: cli_config.index,
+        not_found_uri: cli_config.not_found
+    });
+
+    if setup.is_err() {
+        error!("Error: {}", setup.err().unwrap());
+    }
+}
